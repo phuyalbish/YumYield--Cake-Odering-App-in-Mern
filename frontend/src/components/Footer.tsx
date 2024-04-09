a6b452371dc6bbf5924940fb112de27e23195e7c
Mr Vishope <104910055+phuyalbish@users.noreply.github.com>
2024-04-09 11:50:05 +0545

    Landing Page Done

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <div className="bg-yellow-500 py-5">
+      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center  py-10">
+        <span className="text-3xl text-black font-bold tracking-tight">
+          YumYields.com
+        </span>
+        <span className="text-black font-bold tracking-tight flex gap-4">
+          <span>Privacy Policy</span>
+          <span>Terms of Services</span>
+        </span>
+      </div>
+
+      <span className="container mx-auto flex justify-around">
+        <p className=""> All Rights Reserved. 2024 @ yumyields.com</p>
+      </span>
+    </div>
+  );
+};
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,13 +1,27 @@
 import "../global.css";
-
+import phoneimg from "../assets/phone.png";
+import downloadImg from "../assets/downloadImg.png";
 const HomePage = () => {
   return (
     <div className="flex flex-col gap-12">
-      <div className="bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
+      <div className="bg-white rounded-lg shadow-lg py-8 flex flex-col gap-5 text-center">
         <h1 className="text-5xl font-bold tracking-tight text-orange-600">
           Tuck into a takeaway today
         </h1>
-        <span className="text-xl">Food is ajust a click away!</span>
+        <span className="text-xl">Cake is a just a click away!</span>
+      </div>
+      <div className="grid md:grid-cols-2 gap-5">
+        <img src={phoneimg} alt="" />
+        <div className="flex flex-col items-center justify-center gap-4 text-center">
+          <span className="font-bold text-3xl tracking-tighter">
+            Order takeweaway even faster!
+          </span>
+          <span>
+            Download the YumYields application from either of the store for
+            faster cake odering and personalized recommendations.
+          </span>
+          <img className="w-1/2" src={downloadImg} alt="" />
+        </div>
       </div>
     </div>
   );
